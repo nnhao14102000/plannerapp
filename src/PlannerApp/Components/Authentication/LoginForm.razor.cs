81d863ccc25cacceaa3ca4137ef8aac3d90ae190
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-01-08 20:25:04 +0700

    Implement the Auth Layout & Design the Login Form

---
+++ src/PlannerApp/Components/Authentication/LoginForm.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using PlannerApp.Shared.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Components.Authentication
+{
+    public partial class LoginForm: ComponentBase
+    {
+
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        private LoginRequest _model = new LoginRequest();
+
+        private async Task LoginUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
