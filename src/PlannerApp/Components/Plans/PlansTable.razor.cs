6d64db3bcaa429f23d13f61d59c5a1494c0f10fb
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-01-14 21:08:05 +0700

    Show Plans in a DataGrid using MudBlazor Table with Server Side Reload

---
+++ src/PlannerApp/Components/Plans/PlansTable.razor.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using PlannerApp.Client.Services.Interfaces;
+using PlannerApp.Shared.Models;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Components
+{
+    public partial class PlansTable
+    {
+        [Inject]
+        public IPlansService PlansService { get; set; }
+
+        [Parameter]
+        public EventCallback<PlanSummary> OnViewClicked { get; set; }
+
+        [Parameter]
+        public EventCallback<PlanSummary> OnDeleteClicked { get; set; }
+
+        [Parameter]
+        public EventCallback<PlanSummary> OnEditClicked { get; set; }
+
+        private string _query = string.Empty;
+        private MudTable<PlanSummary> _table;
+
+        private async Task<TableData<PlanSummary>> ServerReloadAsync(TableState state)
+        {
+            var result = await PlansService.GetPlansAsync(_query, state.Page, state.PageSize);
+            return new TableData<PlanSummary>
+            {
+                Items = result.Value.Records,
+                TotalItems = result.Value.ItemsCount
+            };
+        }
+
+        private void OnSearch(string query)
+        {
+            _query = query;
+        }
+
+    }
+}
